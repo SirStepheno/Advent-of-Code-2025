38d53421de21171e11af41e3c86a30edbf85f529
SirStepheno <stefan1752002@gmail.com>
2025-12-01 09:20:58 +0100

    Init

---
+++ Day 0/main.py
@@ -0,0 +1,17 @@
+with open("Day 0/input.txt") as f:
+    lines = [line[:-1] for line in f.readlines()]
+
+def part_one():
+    pass
+
+def part_two():
+    pass
+
+import time
+startTime = time.time()
+
+print(f"Part one: {part_one()}")
+print(f"Part two: {part_two()}")
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
