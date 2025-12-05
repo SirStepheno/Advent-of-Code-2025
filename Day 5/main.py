707944cb31e4c00273d24813956be564f0b4cd00
Stefan de Laat <stefan1752002@gmail.com>
2025-12-05 22:12:47 +0100

    Finished day 5

--- Day 5/main.py
+++ Day 5/main.py
@@ -1,49 +1,47 @@
+def merge_ranges(ranges):
+    new_ranges = []
+    # Add every range to new_range again
+    for r_to_add in ranges:
+        merged = False
+        # Compare new_range to every range already inside, if no overlap, add it to the end, if merge and redo
+        for r_i, r_compare in enumerate(new_ranges):
+            merged_r = range(max(r_to_add[0], r_compare[0]), min(r_to_add[-1], r_compare[-1])+1)
+            if len(merged_r) > 0:
+                new_ranges[r_i] = range(min(r_to_add[0], r_compare[0]), max(r_to_add[-1], r_compare[-1])+1)
+                new_ranges = merge_ranges(new_ranges)
+                merged = True
+                break
+
+        if not merged:
+            new_ranges.append(r_to_add)
+    
+    return new_ranges
+
 with open("Day 5/input.txt") as f:
     lines = [line[:-1] for line in f.readlines()]
     sep = lines.index("")
     ranges_input, foods_input = lines[:sep], lines[sep+1:]
 
-def part_one():
     ranges = []
     for r in ranges_input:
         f, t = map(int, r.split("-"))
         ranges.append(range(f,t+1))
-    
+
+    merged_ranges = merge_ranges(ranges)
+
+    foods_input = [int(food) for food in foods_input]
+
+def part_one():
     res = 0
     for food in foods_input:
-        food = int(food)
-        for r in ranges:
+        for r in merged_ranges:
             if food in r:
                 res += 1
                 break
     return res
 
-def merge_ranges(ranges):
-    new_ranges = []
-    c = 0
-    # Check if range to add overlaps with existing ones
-    for r_to_add in ranges:
-        for r_compare in new_ranges:
-            merged_r = range(max(r_to_add[0], r_compare[0]), min(r_to_add[-1], r_compare[-1])+1)
-            if len(merged_r) > 0:
-                new_ranges.append(merged_r)
-                print(merged_r)
-                c += 1
-            else:
-                new_ranges.append(r)
-
-    if not c:
-        new_ranges.append(input_r)
-
-    return new_ranges
-
 def part_two():
-    ranges = []
-    for r in ranges_input:
-        f, t = map(int, r.split("-"))
-        ranges.append(range(f,t+1))
-    
-    merge_ranges(ranges)
+    return sum([len(r) for r in merged_ranges])
 
 import time
 startTime = time.time()
---
+++ Day 5/test.py
@@ -0,0 +1,6 @@
+a = [1,2,3]
+
+for x in a:
+    x = 3
+
+print(a)
